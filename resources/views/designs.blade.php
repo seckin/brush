6dd6247b1f821063dc4c17aecd3fab59e67b54a2
Seckin Can Sahin <seckincansahin@gmail.com>
2017-04-14 22:03:23 +0300

    all designs page

---
+++ resources/views/designs.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.main')
+
+@section('content')
+<body id="home">
+<!--     <section class="status">
+        <div class="container">
+            <a href="#">Visit our newest collection: Cityscapes</a>
+        </div>
+        <a href="#">
+            <img src="/assets/images/interface/icon-close.png" />
+        </a>
+    </section>
+ -->
+    @include('partials.navbar')
+
+	<section class="showcase">
+	    <div class="container">
+	        <h3>All Designs</h3>
+	        <div class="cards">
+	        	@foreach($designs as $design)
+	            <div class="card item">
+	                <a href="/designs/{{$design->id}}">
+	                    <img src="{{$design->image}}" />
+	                    <p>{{$design->name}}</p>
+	                </a>
+	            </div>
+	            @endforeach
+	        </div>
+	    </div>
+	</section>
+
+	@include('partials.subscribe-to-list')
+
+    <!--
+    <footer>
+        <div class="container">
+            
+        </div>
+    </footer>
+-->
+    <!--
+    <section class="modal">
+        <div class="container">
+            <div class="box">
+                <div class="head">
+                    <b>ONAY GEREKİYOR</b>
+                </div>
+                <div class="body">
+                    <p>Taslağı silmek istediğinizden emin misiniz?</p>
+                    <a class="button">TAMAM</a>
+                    <a class="button reject">İPTAL</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    -->
+</body>
+@endsection
