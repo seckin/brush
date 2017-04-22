4549298c72999e3819baaba192722448b95ccc09
Seckin Can Sahin <seckincansahin@gmail.com>
2017-04-22 13:44:23 +0300

    delete from cart, cart header item toggle, implement cart header item

--- app/CartItem.php
+++ app/CartItem.php
@@ -3,9 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CartItem extends Model
 {
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
     public function design()
     {
         return $this->belongsTo('App\Design');
